929fccb2001b2ae55ef94796d1480fd8a616e315
Name?= null <Ire.Raufu76@gmail.com>
2020-12-22 15:01:16 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,52 @@
+require "json"
+require "httparty"
+
+
+def build_query(query:, name:, values:)
+    q = "{#{query}(#{name}) {"
+    values.each do |item|
+        if item == values[-1]
+            q += "#{item}"
+        else
+            q += "#{item} "
+        end
+    end
+
+    return q += "}}"
+end
+
+
+class User
+    include HTTParty
+    base_uri 'repl.it'
+
+    def initialize(name)
+        q = {
+            :query => build_query(
+                query: "userByUsername",
+                name: "username: '#{name}'",
+                values: ["karma"]
+            )
+        }
+
+        h = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Connection': 'keep-alive',
+            'X-Requested-With':'Repl.it',
+            'Referrer': 'https://repl.it',
+            'Origin': 'https://repl.it'
+        }
+
+        @options = {body: q.to_json, headers: h}
+    end
+
+    def info
+        return self.class.post("/graphql", @options)
+    end
+end
+
+r = User.new("IreTheKID")
+
+puts r.info
